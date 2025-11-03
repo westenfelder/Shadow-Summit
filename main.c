dfea12716db7daa1bb7cf8e063525e815d3d676e
Your Name <you@example.com>
2025-11-03 18:34:39 +0000

    clean-up

--- main.c
+++ main.c
@@ -65,22 +65,17 @@ void xor_string(char *data, long len, char key) {
     }
 }
 
-//churn function, rotat, xor, rotate xor 
-#include <stdint.h>
-#include <stddef.h>
-
 // Rotate left and right for a byte
 static inline uint8_t rol8(uint8_t val, int r_bits) {
     r_bits %= 8;
     return (val << r_bits) | (val >> (8 - r_bits));
 }
-
 static inline uint8_t ror8(uint8_t val, int r_bits) {
     r_bits %= 8;
     return (val >> r_bits) | (val << (8 - r_bits));
 }
 
-// 
+// logic for ensuring the correct flag is provided
 void churn(uint8_t *data, int len, uint8_t key1, uint8_t key2) {
     for (int i = 0; i < len; ++i) {
         // Step 1: rotate right by 13 (mod 8)
@@ -91,43 +86,6 @@ void churn(uint8_t *data, int len, uint8_t key1, uint8_t key2) {
         data[i] = rol8(data[i], 7);
         data[i] ^= key2;
     }
-}
-
-
-ALWAYS_INLINE static uint32_t simple_hash(char *data, int len)
-{
-    const unsigned char *bytes = (const unsigned char *)data;
-    uint32_t hash = 0x9747b28c; // arbitrary seed (can change)
-    uint32_t prime = 0x9e3779b1; // large prime for mixing
-
-    for (int i = 0; i < len; ++i) {
-        hash ^= bytes[i];
-        hash *= prime;
-        hash ^= hash >> 15;
-    }
-
-    // final avalanche
-    hash ^= hash >> 16;
-    hash *= 0x85ebca6b;
-    hash ^= hash >> 13;
-    hash *= 0xc2b2ae35;
-    hash ^= hash >> 16;
-
-    return hash;
-}
-
-void print_int(uint32_t input){
-    char buffer[32] = {0};
-    int i = 30;
-    buffer[31] = '\n'; // newline at the end
-    buffer[30] = '\0';
-
-     while (input > 0 && i > 0) {
-        buffer[i--] = '0' + (input % 10);
-        input /= 10;
-    }
-    sys_write(1, &buffer[i + 1], 11);
-
 }
 
 int main() {
@@ -142,11 +100,6 @@ int main() {
     char read_buffer[128];
     long bytes_read = sys_read(0, read_buffer, 128);
     churn(read_buffer, flag_len, key_1, key_2);
-
-    uint32_t orig_flag = simple_hash(read_buffer, flag_len);
-    uint32_t py_flag = simple_hash(encoded_flag,flag_len);
-    print_int(orig_flag);
-    print_int(py_flag);
 
     if(sys_memcmp(encoded_flag, read_buffer, flag_len) == 0) {
         const char *correct_msg = "Correct Flag\n";
